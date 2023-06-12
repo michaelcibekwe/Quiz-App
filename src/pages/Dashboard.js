c9c5c00afb8928e58d0109c6167e0243809168f9
michaelcibekwe <michaelcibekwe@gmail.com>
2023-06-12 14:33:54 -0500

    added dashboard buttons and score table

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppBar, 
-    Box, 
+    Box,
+    Button, 
     Toolbar, 
     Typography, 
     Container, 
@@ -16,55 +17,126 @@ import { AppBar,
      } from '@mui/material';
 
 const columns = [
-    { id: 'name', label: 'Quiz Name', minWidth: 170 },
-    { id: 'score', label: 'Score', minWidth: 100 },
+    { id: 'name', label: 'Quiz Name', minWidth: 200 },
+    { id: 'score', label: 'Score', minWidth: 25 },
 ];
 
 function createData(name, score) {
     return { name, score}
 }
 
+const rows = [
+    createData('Intro to Science', 100),
+    createData('Chemistry', 90),
+    createData('World History', 98),
+    createData('United States History', 70),
+    createData('Biology', 49),
+    createData('European History', 73),
+    createData('Biology', 45),
+    createData('Statistics', 0),
+    createData('Operating Systems', 87),
+    createData('Philosophy', 50),
+    createData('Politics', 23),
+    createData('Trigonometry', 99),
+    createData('European Culture', 2),
+    createData('Application Development', 94),
+    createData('Operating Systems', 5),
+  ];
 
+  export default function Dashboard() {
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  
+    const handleChangePage = (event, newPage) => {
+      setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = (event) => {
+      setRowsPerPage(+event.target.value);
+      setPage(0);
+    };
 
-function Dashboard() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h6" noWrap>
-            Dashboard
-          </Typography>
-        </Toolbar>
-      </AppBar>
+        <AppBar position="fixed">
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
+                    Dashboard
+                </Typography>
+                <Button color="inherit">New Quiz</Button>
+                <Button color="inherit">Take Quiz</Button>
+            </Toolbar>
+        </AppBar>
       
       <Toolbar />
 
       <Container maxWidth="lg">
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} md={4}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} md={6}>
             <Paper sx={{ p: 2, textAlign: 'center' }}>
               <Typography variant="h6" component="h2">
                 My Quizzes
               </Typography>
               <Typography variant="body2" component="p">
-                Some content for Card 1.
+                Dummy Data
               </Typography>
             </Paper>
           </Grid>
-          <Grid item xs={12} sm={6} md={4}>
+          <Grid item xs={12} sm={6} md={6}>
             <Paper sx={{ p: 2, textAlign: 'center' }}>
               <Typography variant="h6" component="h2">
                 Quiz Scores
               </Typography>
-              <Typography variant="body2" component="p">
-                Some content for Card 2.
-              </Typography>
+                <TableContainer sx={{ maxHeight: 440 }}>
+                    <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                        {columns.map((column) => (
+                            <TableCell
+                            key={column.id}
+                            align={column.align}
+                            style={{ minWidth: column.minWidth }}
+                            >
+                            {column.label}
+                            </TableCell>
+                        ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows
+                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        .map((row) => {
+                            return (
+                            <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                {columns.map((column) => {
+                                const value = row[column.id];
+                                return (
+                                    <TableCell key={column.id} align={column.align}>
+                                    {column.format && typeof value === 'number'
+                                        ? column.format(value)
+                                        : value}
+                                    </TableCell>
+                                );
+                                })}
+                            </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                    </Table>
+        </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
             </Paper>
           </Grid>
         </Grid>
       </Container>
     </Box>
   );
-}
-
-export default Dashboard;
+};
