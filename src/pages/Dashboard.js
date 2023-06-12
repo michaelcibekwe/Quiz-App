df5f77d9d9efc779d7e5aadc57a000e80fa65025
michaelcibekwe <michaelcibekwe@gmail.com>
2023-06-12 11:38:46 -0500

    basic dashboard template

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,41 +1,70 @@
-import React from "react";
-import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import React from 'react';
+import { AppBar, 
+    Box, 
+    Toolbar, 
+    Typography, 
+    Container, 
+    Grid, 
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TablePagination,
+    TableRow
+     } from '@mui/material';
 
-const Dashboard = () => {
-    return (
-        <Box 
-            sx={{
-                width: 300,
-                height: 300,
-                backgroundColor: 'primary.dark',
-            }}
-        >
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Header 1</TableCell>
-                        <TableCell>Header 2</TableCell>
-                        <TableCell>Header 3</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    <TableRow>
-                        <TableCell>Row 1, Cell 1</TableCell>
-                        <TableCell>Row 1, Cell 2</TableCell>
-                        <TableCell>Row 1, Cell 3</TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>Row 2, Cell 1</TableCell>
-                        <TableCell>Row 2, Cell 2</TableCell>
-                        <TableCell>Row 2, Cell 3</TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-      </TableContainer>
+const columns = [
+    { id: 'name', label: 'Quiz Name', minWidth: 170 },
+    { id: 'score', label: 'Score', minWidth: 100 },
+];
+
+function createData(name, score) {
+    return { name, score}
+}
+
+
+
+function Dashboard() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      
+      <Toolbar />
+
+      <Container maxWidth="lg">
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper sx={{ p: 2, textAlign: 'center' }}>
+              <Typography variant="h6" component="h2">
+                My Quizzes
+              </Typography>
+              <Typography variant="body2" component="p">
+                Some content for Card 1.
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Paper sx={{ p: 2, textAlign: 'center' }}>
+              <Typography variant="h6" component="h2">
+                Quiz Scores
+              </Typography>
+              <Typography variant="body2" component="p">
+                Some content for Card 2.
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </Box>
-    );
-};
-
+  );
+}
 
 export default Dashboard;
